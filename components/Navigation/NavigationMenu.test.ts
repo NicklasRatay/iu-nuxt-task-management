6c5f35dfb6ba66d6adec98d51b87bbc0b46bd56b
Nicklas Ratay <nicklas.ratay@gmail.com>
2024-12-08 09:57:43 +0100

    Added navigation menu

---
+++ components/Navigation/NavigationMenu.test.ts
@@ -0,0 +1,29 @@
+import { it, expect, describe, beforeEach, afterEach, vi } from "vitest";
+import { mount } from "@vue/test-utils";
+import type { VueWrapper } from "@vue/test-utils";
+import NavigationMenu from "./NavigationMenu.vue";
+
+describe("NavigationMenu", () => {
+    let wrapper: VueWrapper;
+
+    beforeEach(() => {
+        wrapper = mount(NavigationMenu);
+    });
+
+    afterEach(() => {
+        wrapper.unmount();
+    });
+
+    it("can be mounted", () => {
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    it("has correct labels", () => {
+        const entries = wrapper.findAll(
+            ".p-panelmenu-header-content > a > div",
+        );
+
+        expect(entries[0].exists()).toBe(true);
+        expect(entries[0].text()).toBe("Dashboard");
+    });
+});
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,67 @@
 <template>
     <div>
         <Toast />
-        <slot />
+        <div
+            class="fixed z-[999] h-[3.5rem] w-full flex justify-between items-center bg-primary p-2"
+        >
+            <div class="flex gap-2 items-baseline">
+                <Button
+                    icon="pi pi-bars"
+                    @click="isSidebarVisible = !isSidebarVisible"
+                />
+                <NuxtLink
+                    to="/"
+                    class="!text-primary-contrast text-2xl font-semibold"
+                >
+                    {{ APP_NAME }}
+                </NuxtLink>
+            </div>
+        </div>
+        <div
+            v-if="!isMobile"
+            :class="[
+                'fixed z-[998] top-[3.5rem] bottom-0 w-[16rem] overflow-y-auto c-bg-region border-r c-border-color !p-2 transition-[margin transform] ease-in-out duration-300',
+                !isSidebarVisible ? '-translate-x-full' : '',
+            ]"
+        >
+            <NavigationMenu />
+        </div>
+        <Drawer
+            v-if="isMobile"
+            v-model:visible="isSidebarVisible"
+            class="!w-[15rem] overflow-y-auto"
+        >
+            <template #container>
+                <NavigationMenu
+                    class="p-2"
+                    @item-clicked="isSidebarVisible = !isSidebarVisible"
+                />
+            </template>
+        </Drawer>
+        <div
+            v-focustrap
+            :class="[
+                'w-full pt-[5rem] pr-[1.5rem] pb-[1.5rem] transition-[padding] ease-in-out duration-300',
+                !isMobile && isSidebarVisible ? 'pl-[17.5rem]' : 'pl-[1.5rem]',
+            ]"
+        >
+            <slot />
+        </div>
+        <ScrollTop />
     </div>
 </template>
+
+<script setup lang="ts">
+import { APP_NAME } from "~/utils/constants";
+
+const isSidebarVisible = ref(false);
+
+const { width } = useWindowSize();
+const isMobile = computed(() => width.value <= 768);
+
+onMounted(() => {
+    if (!isMobile.value) {
+        isSidebarVisible.value = true;
+    }
+});
+</script>
