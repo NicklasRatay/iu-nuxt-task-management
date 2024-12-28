80e1f69fa16fa31784dcdd984456512cf1d3a671
Nicklas Ratay <nicklas.ratay@gmail.com>
2024-12-28 11:30:28 +0100

    Cleanup

--- supabase/tests/functions.test.sql
+++ supabase/tests/functions.test.sql
@@ -1,10 +1,12 @@
 BEGIN;
 
-SELECT plan(3);
+SELECT plan(5);
 
 SELECT has_function('public', 'test_audit_columns', ARRAY['text', 'text'], 'test_audit_columns(text, text) function exists');
 SELECT has_function('public', 'populate_audit_columns', ARRAY[NULL], 'populate_audit_columns() function exists');
 SELECT has_function('public', 'create_audit_trigger', ARRAY['text', 'text'], 'create_audit_trigger(text, text) function exists');
+SELECT has_function('public', 'get_total_project_count', ARRAY[NULL], 'get_total_project_count() function exists');
+SELECT has_function('public', 'get_total_task_count', ARRAY[NULL], 'get_total_task_count() function exists');
 
 SELECT * FROM finish();
 
